2f297f9babdebe301182d60a54cb5e1a818a404e
Izida Dolotina <46350814+dolotinaiCGI@users.noreply.github.com>
2019-01-24 13:29:56 +0100

    Update timesheet_steps.java

--- src/test/java/steps/timesheet_steps.java
+++ src/test/java/steps/timesheet_steps.java
@@ -1,30 +1,31 @@
 package steps;
 
 import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import cucumber.api.DataTable;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 import framework.DriverUtil;
-import framework.Helpers;
 
 public class timesheet_steps {
 	
 	// get the driver:
 	WebDriver browser = DriverUtil.getDriver();
 	
-	@Given("^User is logged in the timesheet application$")
-	public void user_is_logged_in_the_timesheet_application() throws Exception {
+	@Given("^User is logged in the timesheet application with specified credentails$")
+	public void user_is_logged_in_the_timesheet_application_with_specified_credentails(DataTable credentialsTable) throws Exception {
 	    		
-		// get the url and credentials from the classified.txt file 
-		ArrayList<String> classifiedInfo = Helpers.getClassifiedInformation();
+		// get the credentials data from scenario
+		List<List<String>> data = credentialsTable.raw();
 		
-		String timesheetUrl = classifiedInfo.get(0);
-		String username = classifiedInfo.get(1);
-		String password = classifiedInfo.get(2);
-		
+		String timesheetUrl = data.get(1).get(0);
+		String username = data.get(1).get(1);
+		String password = data.get(1).get(2);
+				
 		// navigate to timesheet login page
 		browser.get(timesheetUrl);
 		
@@ -48,5 +49,74 @@ public class timesheet_steps {
 		WebElement buttonReports = browser.findElement(By.id("win0groupletPTNUI_LAND_REC_GROUPLET$0"));
 		assertTrue(buttonReports.isDisplayed());
 		
+		
 	}
+	
+    @When("^User navigates to CGI Timesheet Reports$")
+    public void user_navigates_to_CGI_Timesheet_Reports() {
+
+           //validate login successful
+           WebElement img = browser.findElement(By.id("PS_REPORT_TIME_L_FL$0"));
+           assertTrue(img.isDisplayed());
+
+           //click on new timesheet
+           img.click();
+    }
+    
+    @And("^User fill main time report details$")
+    public void user_fill_main_time_report_details(DataTable dataTable) throws InterruptedException  {
+
+           //get data from table 
+           List<List<String>> data = dataTable.raw();
+           String EmpID = data.get(1).get(0);
+           String period = data.get(1).get(1);
+
+    browser.switchTo().frame(browser.findElement(By.id("main_target_win0")));
+
+    }
+  //find EmpID 
+    WebElement inputEmpId = browser.findElement(By.name("EX_TIME_ADD_VW_EMPLID"));
+
+    //fill in EmpID
+    inputEmpId.clear();
+    inputEmpId.sendKeys(EmpId);
+
+    //find period
+    WebElement inputPeriod = browser.findElement(By.id("EX_TIME_ADD_VW_PERIOD_END_DT"));
+
+    //set period date
+    inputPeriod.clear();
+    inputPeriod.sendKeys(period);
+
+    //find a Add button
+    WebElement addButton = browser.findElement(By.id("#ICSearch"));
+
+    //click Add button
+    addButton.click();
+
+
+}
+
+@And("^User creates a blank time report$")
+public void user_creates_a_blank_time_report_ID() throws Exception {
+    // find open blank ink
+    WebElement blankLink = browser.findElement(By.id("EX_ICLIENT_WRK_OK_PB"));
+
+    blankLink.click();
+}
+
+@And("^User fill project hours details$")
+public void user_fill_project_hours_details_ID(DataTable projectTable) throws Exception {
+
+    //get data for project hours
+    List<List<String>> data = projectTable.raw();
+    String project = data.get(1).get(0);
+    String activity = data.get(1).get(1);  
+    String category = data.get(1).get(2); 
+    String monday = data.get(1).get(3); 
+    String tuesday = data.get(1).get(4); 
+    String wednesday = data.get(1).get(5); 
+    String thursday = data.get(1).get(6);
+    String friday = data.get(1).get(7);
+}
 }
